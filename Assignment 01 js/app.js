2ef3f50aa68de64c07bf9a92b8d6286b4b66ab7f
Saad Ahmed Khan <saadahmedkhan578@gmail.com>
2024-09-14 23:13:58 +0500

    Add Assignment js

---
+++ Assignment 01 js/app.js
@@ -0,0 +1,84 @@
+// chapter 01 //
+// alert("");
+// alert("Error Please Enter A Valid Password");
+// alert("Welcome Too JS Land \n Happy Coding!");
+// alert("Welcome Too JS Land");
+// alert("Happy Coding..\n Prevent This Page From Creating Additional Dialogs");
+
+
+// Chapter 02 //
+
+// var UserName;
+// alert(UserName)
+// var MyName = "Saad Ahmed Khan"
+// alert(MyName)
+// var message;
+// message = "Hello World"
+// alert(message)
+
+// var studentname = "johndoe"
+// var studentage = "15 Years Old"
+// var studentcourse = "Certified Mobile Application Developement"
+// alert(studentname)
+// alert(studentage)
+// alert(studentcourse)
+// var food = "pizza \n pizz \n piz \n pi \n p"
+// alert(food)
+// var email = "saadahmedkhan578@gmail.com"
+// alert("My Email Adress Is " + email)
+// var book = "A Smarter Way Too learn JavaScript"
+// alert("I Am Trying Too Learn From The Book " + book)
+// document.write("Yah! I can Write HTML Content Through JavaScript")
+// var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(symbol)
+
+// Chapter 03 //
+
+// var age = 15
+// alert("I am " + age + " Years Old")
+
+// var birthyear = 2005
+// document.write("My Birth Year IS " + birthyear)
+
+// var visitorname = "Saad"
+// Producttitle = "T-shirts"
+// Quantity = "15"
+// document.write(visitorname + " Ordered " + Quantity + Producttitle + " On J. Online Store ")  
+
+
+
+// Chapter04 //
+
+
+// var legalNames = [
+//     "myVariable",
+//     "_variable",
+//     "$variable",
+//     "variable1",
+//     "variable_name"
+// ];
+
+// // Illegal variable names
+// var illegalNames = [
+//     "1variable", // Cannot start with a number
+//     "my variable", // Cannot contain spaces
+//     "@variable", // Cannot contain special characters other than $ and _
+//     "var", // Reserved keyword
+//     "function" // Reserved keyword
+// ]
+// document.write("Rules for Naming JS Variables\n" +
+//           "Variable names can only contain letters, numbers, _, and $.\n" +
+//           "Variables must begin with a letter, $, or _. For example: $name, _name, or name.\n" +
+//           "Variable names are case sensitive.\n" +
+//           "Variable names should not be JS reserved keywords.\n");
+// document.write(" legal Names  \n " + legalNames + " illegal Names \n" + illegalNames)
+
+
+
+// Chapter05 //
+var num1 = 3
+var num2 = 5
+var num = num1 + num2
+document.write("The Sum Off " + num1 + "And " + num2 + "is " + num)
+
+

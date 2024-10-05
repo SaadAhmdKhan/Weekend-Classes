28d4ae98cbc0185daa52737a5354487742681bed
Saad Ahmed Khan <saadahmedkhan578@gmail.com>
2024-10-05 23:16:23 +0500

    Add Commit

---
+++ PRACTICE-js/app.js
@@ -0,0 +1,671 @@
+                                                          // ALERT
+// alert("Congratulations \n\n\n\n\n\n\n\n\n\n\n You Have passed the css quiz")
+// alert('Congratulations \n\n\n\n\n\n\n You HAve Passed The Css quiz')
+// alert('Congratulations \n\n\n\n\n\n\n\n\n\n You Have Passed The Css Quiz')
+
+// window.alert('saylani\n saylan\n sayla \n sayl \n say \n sa \n s')
+
+
+// String == "Sualeh"
+// Number / Integer === 2 , 3 235,
+// Boolean === TRUE OR FALSE
+
+// var myname = 'saad'
+//     mynationality = 'pakistan'
+
+// alert("My name is " + myname +  '\n  My nationality is ' + mynationality)
+
+
+// var myname = 'saad'
+// var city = 'karachi'
+
+// alert('My name is ' + myname + ' \n My city is ' + city)
+
+// var myname = 'saad'
+//     city = 'Karachi'
+// alert('my name is ' + myname + 'My city is ' + city)
+
+
+// var num1 = 
+//     num1 = 5
+//     num2 = 5
+//     num3 = '10'
+
+// alert(num1 + num2 + num3)
+
+// var myfirstname = 'saad'    // camel case
+
+
+// var my_second_name = 'saad'   // snak case
+
+// var myname;         // declare
+//     myname = 'saad'
+//     city = 'karachi'
+
+// document.write('My Name Is ' + myfirstname + ' My City Is ' + city)       // this print data in your browser main window
+
+
+
+// alert(false)
+
+// alert('String Data Here')
+
+
+// "Sualeh@1233412212@##%^*" - String
+// 123456780.5582465   - Number / Integer
+// True - False       - Boolean
+
+
+
+// ************ VARAIBLE FOR NUMBER **********************
+
+
+
+// var num = "A15"
+//     num1 = 10
+//     result = num + num1
+//     alert(result)                //only plus sign work all sign are show below NAN
+
+
+
+// var num = "15"
+//     num1 = 10
+//     result = num * num1
+//     alert(result) 
+
+
+// 15 + 15 = "30"
+// "15" + 15 = "1515"
+// "50" - 10 = "40"
+// "15" + "10" = "1510"
+// "AB1" + 10 = "NaN"
+// "AB3" + '10' = "AB310"
+// "AB3" - '10' = "NaN"
+
+
+// function getmyname() {
+//     console.log('saad')
+// }
+
+// var a = 5
+//     b = 10
+// console.log(--b +a - ++a + b++)
+    
+// var num = 2
+//     num1 = 2
+//     num2 = 10
+//     result = (10 + 50) * 10 + (50 - 4)
+//     alert(result)
+
+
+                                          // --num == pre decrement
+                                          // num-- == post decrement
+                                          // ++num == pre increment
+                                          // num++ == post increment
+
+
+// var firstname = 'saad'
+//     secondname = 'khan'
+// console.log(firstname + ' ' + secondname)
+
+
+// var num = +prompt('Please Enter First Num')
+//     num2 = +prompt('Please Enter Second Num')
+
+// document.write('The MultiPly Off ' + num  + 'And ' + num2 + "is" + (num * num2) + "<br>")
+// document.write('The MultiPly Off ' + num  + 'And ' + num2 + "is" + (num * num2) )
+
+// var num = 5
+//     num1 = 10
+ 
+// var celsius = 25
+// var celsiusintoferinheit = (celsius * 9 / 5) + 32
+// console.log(celsiusintoferinheit)   
+
+// var a = 2 
+//     b = 2 
+//     result = --a + ++b - b-- + ++b + a++
+//     alert(result)
+
+// var num = 4
+
+// document.write("<h1> The Table Off " + num + "tasas" + num +"sasa" + "</h1>")
+
+// var subjectone = prompt('Enter Subject One')
+//     subjectonemarks = +prompt('Enter The Marks Obtained In ' + subjectone)
+//     subjecttwo = prompt('Enter Subject Two ')
+//     subjecttwomarks = +prompt('Enter The Marks Obtained In ' + subjecttwo)
+//     subjectthree = prompt('Enter Subject Three')
+//     subjectthreemarks = +prompt('Enter The Marks Obtained In ' + subjectthree)
+
+// var eachsubtotal = 100
+//     subjectoneper = (subjectonemarks / eachsubtotal) * 100
+//     subjecttwoper = (subjecttwomarks / eachsubtotal) * 100
+//     subjectthreeper = (subjectthreemarks / eachsubtotal) * 100
+//     totalmarks = 300
+//     totalobtainedmarks = subjectonemarks + subjecttwomarks + subjectthreemarks
+//     finalper = (totalobtainedmarks / totalmarks) * 100
+
+// document.write(`
+//               <table> 
+//               <tr>
+//               <th> Subject </th>
+//               <th> Total </th>
+//               <th> Obtained </th>
+//               <th> Percentage </th>
+//               </tr>
+
+//               <tr>
+
+//               <td> ${subjectone} </td>
+//               <td> ${eachsubtotal} </td>
+//               <td> ${subjectonemarks} </td>
+//               <td> ${subjectoneper} </td>
+//               </tr>
+             
+//              <tr>
+//              <td> ${subjecttwo} </td>
+//              <td> ${eachsubtotal} </td>
+//              <td> ${subjecttwomarks} </td>
+//              <td> ${subjecttwoper} </td>
+//              </tr>
+             
+//              <tr> 
+//              <td> ${subjectthree} </td>
+//              <td> ${eachsubtotal} </td>
+//              <td> ${subjectthreemarks} </td>
+//              <td> ${subjectthreeper}  </td>                   
+//              </tr>
+//              <tr>
+//                        <th> Total </th>
+//                           <th> ${totalmarks} </th>
+//                           <th> ${totalobtainedmarks} </th>
+//                           <th> ${finalper}% </th>
+//                 </tr>
+             
+             
+//              </table>
+
+
+// `)
+
+
+// var userage = +prompt('Enter Your Age')
+
+// if(userage === 20 ){
+//     alert('You Are Eligible')
+
+// }
+
+// var kchlayao = prompt("kch lay kr ao")
+// if(kchlayao === "dahi"){
+//     alert("lay ao");
+// }
+// else if(kchlayao === "chips"){
+//     alert("2 lay ao");
+// }
+// else if(kchlayao === "milk"){
+//     alert("2 kg lay ao");    
+// }
+// else{
+//     alert("kch bee lay ao khali hath mt ana");
+// }
+
+
+// var num = 20
+    // num1 = "20"                         check the bolean value
+// alert(num === num1)
+
+// var userage = +prompt("Enter Your Age")
+
+// if(userage <17){
+//     alert("you are not eligible")
+// }
+// else{
+//     alert("You are eligible for cnic")
+// }
+
+
+// var usercity = prompt("Enter Your City")
+// if(usercity === "karachi"){
+//     alert("biryani")
+// }
+// else if(usercity === "islamabad"){
+//     alert("dambuk")
+// }
+// else if(usercity === "lahore"){
+//     alert("paye")
+// }
+// else if(usercity === "peshawar"){
+//     alert("qorma")
+// } 
+// else{
+//     alert('samose')
+// }
+
+// var ticketnum = +prompt("Enter Your Ticket Num")
+
+// if(ticketnum < 1000){
+//     alert('your show day is monday')
+// }
+// else if(ticketnum <= 2000){
+//     alert('your show day is tuesday')
+// }
+// else if(ticketnum <= 3000){
+//     alert('your show day is wednesday')
+// }
+// else{
+//     alerty("your show day is thursday")
+// }
+
+// var rollnum = +prompt('Enter Your Roll Num')
+// if(rollnum !== 3564){
+//     alert("you can take test")
+// }
+// else{
+//     alert("you are eliminate")
+// }
+
+
+// var userInput = +prompt('Enter your Age')
+// alert(typeof(userInput))
+
+
+
+// var a = 2
+//     b = 3
+//     result = --a + ++b + b++ + --b
+        //     1  +  4 + 4 +     4
+
+// alert(result)
+
+// var totalcost = 1 + 3 * (4) 3 4 za 12 + 1 = 13
+// alert(totalcost)
+
+// var userName = 'John'
+
+// document.write("<h1>" + "My Name is " + userName + "</h1>")
+
+// var rollno = 123
+//     percentage = 65
+//     username = prompt('Enter Your Name' , 'Ali')  
+// document.write(`
+//         <table>
+//         <tr> 
+        
+//         <th> NAme  </th>
+//         <th> Rollnum </th>
+//         <th> Percentage </th>
+//         </tr>        
+        
+
+//         <tr> 
+//         <th> ${username} </th>
+//         <th> ${rollno} </th>
+//         <th> ${percentage} </th> 
+//         </tr>
+        
+        
+        
+        
+//  </table>
+
+// `)    
+
+
+// var usernum = +prompt('Enter Number For Multiplication' , 5)
+//     alert(usernum + usernum)
+
+// document.write(`<p> ${usernum} * 1 = ${usernum * 1}`)
+
+// var num = 2
+//     num1 = 2
+
+//     alert(num === num1)
+
+// var num = 10
+//     num1 = "10"
+//     alert(typeof(num1))      type off means check data types
+
+// var city = "KarAChi"
+//     city1 = 'karachi'
+// alert(city !== city1)
+
+
+// var num2 = 10
+// alert(num2 <= 10)
+
+
+// var usercity = prompt('Enter Your City Name')
+// if(usercity === "karachi"){
+// alert("You belong to the city off lights")
+// }
+// else {
+//     alert("you belong to just a normal city")
+// }
+
+
+// var userage = +prompt("Enter YOur Age")
+// if(userage <=10){
+//     alert('You are just a kid')
+// }
+// else if (userage <=20){
+//     alert('You are teen ager')
+// }
+// else if (userage <=30){
+//     alert('You Are Young Man')
+// }
+// else if (userage <=50){
+//     alert('You Are Closed Too Retirement')
+// }
+// else{
+//     alert('allah allah karo')
+// }
+
+// var num = 0.25
+// alert(num <0.50)
+
+// var num = 4
+//     userval = +prompt("Enter Number")                sir say inform
+//     if(userval === num){
+
+//     }
+// else if (userval === ++num)
+
+// var percentage = 83
+// var Grade;
+// var Remarks;
+
+// if(percentage >=80){
+//    Grade = "A+"
+//    Remarks = "Excellent" 
+// }    
+// else if (percentage >=70){
+//     Grade = "A+"
+//     Remarks = "Excellent"
+// }
+// document.write(`
+//  The Grade Is ${Grade}
+//  <br/>
+// `)
+
+
+// var a = 2
+//     b = 2
+//     // result = b++ assitice               postincrement
+//     // result = b-- assitice               postincrement
+        //result = --b one num less           predecrement
+//     // result = ++b one num add            preincrement
+
+// var a = 2
+//     b = 2
+//     result = --b
+//     alert(result)
+//     document.write(`The Value Off --b Is ${result} <br /> <br />`)
+
+// var a = 2
+//     b = 1
+//     result = --a - --b
+//     alert(result)
+// document.write(`The Value Off --a - --b Is ${result} <br /> <br />`)
+
+// var a = 2
+//     b = 1
+//     result = --a - --b + ++b + b--
+// alert(result)
+// document.write(`The Value Off --a - --b + ++b + b-- Is ${result}`)
+
+// var username =prompt('Enter Your Username')
+// alert("Hello " + username)
+
+// var usernum =+prompt('Please Enter Your Usernumber')
+// alert('Your Number Is ' + usernum)
+
+var usernum = +prompt('Please Enter Any Number ' , 5)
+
+document.write(`
+
+             <h1> Table Off ${usernum}   </h1> <br /> <br />
+             <p> ${usernum} * 1 = ${usernum * 1}</p>
+             <p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert("ThanksForYourInput!")
+// var username = "saad"
+// username = "sharjeel";
+// username = "Mahnoor"
+// alert(username)
+
+// var weight = 150
+// alert(weight+25)
+// var orgnlnum = 23
+// var newnum = orgnlnum + 7
+// alert(newnum)
+
+// var orgnlnum = 23
+// var numtobeadded = 7
+// var newnum = orgnlnum + numtobeadded;
+// alert(newnum)
+
+// var orgnlnum = "90"
+
+// orgnlnum = orgnlnum + 10
+// alert(orgnlnum)
+
+// var orgnlnum1 = "23"
+// var newnum = orgnlnum1 + 7
+// alert(newnum)
+
+                                            // MATH EXPRESSIONS FAMILIAR OPERATERS
+
+// var popularnum = 4
+// var popularnum = 2+2
+// alert(popularnum)
+
+// var popularnum = 12-24
+// alert(popularnum)
+
+// var popularnum = 3*12
+// alert(popularnum)
+
+// var num = 10
+// var popularnum = num+200
+// alert(popularnum)
+
+// var num = 10
+// var anothernum = 1
+// var popularnum = num+anothernum
+// alert(popularnum)
+
+// THIS ONE ISNOT FAMILIAR IT IS CALLED MODULOUS ITS RETURN 0 
+
+// var whatsleftover = 10 % 3
+// alert(whatsleftover)
+
+// var whatsleftover = 9 % 3
+// alert(whatsleftover)
+
+                                        // MATH EXPRESSIONS UNFAMILIAR OPERATERS  
+
+// var num = 1
+// var newnum = num++
+// alert(newnum)                                        
+
+// var num = 1
+// var newnum = ++num
+// alert(newnum)
+
+// var num = 1
+// var newnum = num--
+// alert(newnum)
+
+// var num = 1
+// var newnum = --num
+// alert(newnum)
+
+                                       
+                                                // ELIMINATING AMBAGUITY CH # 07
+
+// var totalcost = (1 + 3) * 4
+// alert(totalcost)
+
+
+// var totalcost = 1 + (3 * 4)
+// alert(totalcost)   
+
+// var resultoffcomputation = (2 * 4) * 4 + 2
+
+// alert(resultoffcomputation)
+
+// var resultoffcomputation = 2 * 4 * (4+2)
+// alert(resultoffcomputation)
+
+                                  // The solution is more parentheses.
+// var resultoffcomputation = ((2 * 4) * 4) + 2
+// alert(resultoffcomputation)
+
+
+// var resultoffcomputation = (2 * 4) * (2 + 4)
+// alert(resultoffcomputation)
+
+
+                                            //  CONCATENATING TEXT STRING CH # 08
+
+// var username = "Susan"
+// alert("Thanks " + "Susan " + "!")
+
+// var message = "Thanks"
+// var username = "Susan"
+// var Banger = "!"
+// alert(message + username + Banger)
+// var message = "Welcome "
+//  username = "Saad "
+//  Banger = "!"
+//  custommess = (message + username + Banger) 
+// alert(custommess)
+// var num = "2"
+//  num1 = "2"
+// alert(num + num1)
+// alert ("2 plus 2 equals " + 2 + 2)
+
+
+
+                                                    // PROMPTS CH#09
+
+// var specs =prompt("Your Species?" "Human")
+// alert(specs)
+// var question = "What Your Species?"
+// var defaultanswer = "Human"
+// var spec =prompt(question , defaultanswer)
+
+// var numofcats = prompt("How Many Cats?")
+// var toomanycats  = numofcats + 1 
+
+
+
+                                                  // IF STATEMENTS 
+
+
+// var x = prompt("Where Does The Pop live?")
+//    if (x === "vatican"){
+//     alert("Correct!")
+
+//    }
+
+// var x = prompt("Where Is The Pop Lives")
+
+// if( x === correctanswer ){
+//     score++;
+//     userique = ("genius");
+//     alert("correct");
+// }
+
+
+

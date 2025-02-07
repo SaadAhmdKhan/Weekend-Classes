a1b6d860b49b4723e7ee60483f73b01e744fa7d4
Saad Ahmed Khan <saadahmedkhan578@gmail.com>
2025-02-08 03:03:00 +0500

    Add work

--- Password_Generator/app.js
+++ Password_Generator/app.js
@@ -1,1 +1,8 @@
-alert("Hello User")
+function randompasswordgenerator(){
+    var randomstring='asdfghjkl1234567890/mnbvcxz!@#$%&poiyyt6789jhgffdmnbvcxzaqwetyuiioASDEWRTYGFDDERTTTFVVCXTTUIKKIMKM'
+    var randompassword=''
+  for(i=0;i<7;i++){
+    var randomnum = Math.floor(Math.random()*randomstring.length-1)
+      randompassword+= randomstring[randomnum] }
+  document.getElementById("input-range").innerHTML= `<span>${randompassword}</span>`
+  }
